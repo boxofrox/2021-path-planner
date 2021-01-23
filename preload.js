928fee5e0a4c8a5afa2fe256aad2384ef558b8e2
Emmie <affordablehaulingandtransport@gmail.com>
2021-01-23 17:05:16 -0500

    Remove starter code
    - Emmie

--- preload.js
+++ preload.js
@@ -3,17 +3,7 @@ const fieldimage = new Image();
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-
   const canvas = document.getElementById('canvas');
- 
 
   fieldimage.src = 'images/field.png';
   fieldimage.addEventListener('load', (ev) => {
